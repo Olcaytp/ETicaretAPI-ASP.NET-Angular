37048c0c77e45e9387020907b0fee87c851c39de
olcay <olcaytapan10@gmail.com>
2023-04-14 15:18:15 +0200

    product-listing-pagination-21

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.RequestParameters;
 using ETicaretAPI.Application.ViewModels.Products;
 using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -37,9 +38,24 @@ namespace ETicaretAPI.API.Controllers
         }
         [HttpGet]
 
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]Pagination pagination)
         {
-            return Ok(_productReadRepository.GetAll(false));
+           var totalCount = _productReadRepository.GetAll(false).Count();
+           var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Price,
+                p.Stock,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).ToList();
+            //take-skip yapısı ile sayfalama yapılabilir. 3.sayfada 5 eleman varsa 15. elemandan başlayıp 5 eleman getirir.
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
         }
 
         [HttpGet("{id}")]
@@ -53,11 +69,6 @@ namespace ETicaretAPI.API.Controllers
 
         public async Task<IActionResult> Post(VM_Create_Product model)
         {
-            if (ModelState.IsValid)
-            {
-                
-            }
-
             await _productWriteRepository.AddAsync(new()
             {
                 Name = model.Name,
