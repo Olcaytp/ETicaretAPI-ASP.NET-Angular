30eec3b091e71869e6283bbf4ef579e05aecd68d
olcay <olcaytapan10@gmail.com>
2023-04-24 09:29:34 +0200

    blob-storage-service-entegration-31

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -130,7 +130,8 @@ _customerWriteRepository = customerWriteRepository; */
         [HttpPost("[action]")]
         public async Task<IActionResult> Upload()
         {
-            var datas = await _storageService.UploadAsync("resource/files", Request.Form.Files);
+            var datas = await _storageService.UploadAsync("files", Request.Form.Files); // Azure Blob Storage
+            //var datas = await _storageService.UploadAsync("resource/files", Request.Form.Files);
             //var datas = await _fileService.UploadAsync("resource/product-images", Request.Form.Files);
             await _productImageFileWriteRepository.AddRangeAsync(datas.Select(d => new ProductImageFile()
             {
