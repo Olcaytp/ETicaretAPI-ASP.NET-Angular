c256f6379156af4a85c5acc1a67d49a7ede9a22d
olcay <olcaytapan10@gmail.com>
2023-05-04 20:06:38 +0200

    asp.netcore-identity-mechanism-created-38

---
+++ Core/ETicaretAPI.Application/Exceptions/UserCreateFailedExceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Exceptions
+{
+    public class UserCreateFailedExceptions : Exception
+    {
+        public UserCreateFailedExceptions(string? message) : base(message)
+        {
+        }
+
+        public UserCreateFailedExceptions(string? message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public UserCreateFailedExceptions() : base("User Create Failed")
+        {
+        }
+    }
+}
