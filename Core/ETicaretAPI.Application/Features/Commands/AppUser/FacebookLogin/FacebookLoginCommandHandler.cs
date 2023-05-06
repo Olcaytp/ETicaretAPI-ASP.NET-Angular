facc02d232582b52b91c47713a9b28b87b72440a
olcay <olcaytapan10@gmail.com>
2023-05-06 15:39:08 +0200

    facebook-login-processes-44

---
+++ Core/ETicaretAPI.Application/Features/Commands/AppUser/FacebookLogin/FacebookLoginCommandHandler.cs
@@ -0,0 +1,77 @@
+﻿using ETicaretAPI.Application.Abstraction.Token;
+using ETicaretAPI.Application.DTOs;
+using ETicaretAPI.Application.DTOs.Facebook;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin
+{
+    public class FacebookLoginCommandHandler : IRequestHandler<FacebookLoginCommandRequest, FacebookLoginCommandResponse>
+    {
+        readonly UserManager<Domain.Entities.Identity.AppUser> _userManager;
+        readonly ITokenHandler _tokenHandler;
+        readonly HttpClient _httpClient;
+
+        public FacebookLoginCommandHandler(IHttpClientFactory httpClientFactory, ITokenHandler tokenHandler, UserManager<Domain.Entities.Identity.AppUser> userManager)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _tokenHandler = tokenHandler;
+            _userManager = userManager;
+        }
+
+        public async Task<FacebookLoginCommandResponse> Handle(FacebookLoginCommandRequest request, CancellationToken cancellationToken)
+        {
+            string accessTokenResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/oauth/access_token?client_id=1443621459796844&client_secret=81a885c33970101978b2ffcefaae8732&grant_type=client_credentials");
+
+            FacebookAccessTokenResponse facebookAccessTokenResponse = JsonSerializer.Deserialize<FacebookAccessTokenResponse>(accessTokenResponse);
+
+            string userAccessTokenValidation = await _httpClient.GetStringAsync($"https://graph.facebook.com/debug_token?input_token={request.AuthToken}&access_token={facebookAccessTokenResponse.AccessToken}");
+
+            FacebookUserAccessTokenValidation validation = JsonSerializer.Deserialize<FacebookUserAccessTokenValidation>(userAccessTokenValidation);
+
+            if (validation.Data.IsValid) {
+                string userInfoResponse = await _httpClient.GetStringAsync($"https://graph.facebook.com/me?fields=email,name&access_token={request.AuthToken}");
+
+                FacebookUserInfoResponse userInfo = JsonSerializer.Deserialize<FacebookUserInfoResponse>(userInfoResponse);
+
+                var info = new UserLoginInfo("FACEBOOK", validation.Data.UserId, "FACEBOOK");
+                Domain.Entities.Identity.AppUser user = await _userManager.FindByLoginAsync(info.LoginProvider, info.ProviderKey);
+
+                bool result = user != null;
+                if (user == null)
+                {
+                    user = await _userManager.FindByEmailAsync(userInfo.Email);
+                    if (user == null)
+                    {
+                        user = new()
+                        {
+                            Id = Guid.NewGuid().ToString(),
+                            Email = userInfo.Email,
+                            UserName = userInfo.Email,
+                            NamSurname = userInfo.Name,
+                        };
+                        var identityResult = await _userManager.CreateAsync(user);
+                        result = identityResult.Succeeded;
+                    }
+                }
+                if (result)
+                {
+                    await _userManager.AddLoginAsync(user, info); //AspnetUserLogins
+                    Token token = _tokenHandler.CreateAccessToken(300);
+
+                    return new()
+                    {
+                        Token = token
+                    };
+                }
+            }
+            throw new Exception("Facebook ile giriş başarısız");
+        }
+    }
+}
