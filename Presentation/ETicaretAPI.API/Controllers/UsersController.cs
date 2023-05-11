5ac84a00d340b61eca33e8b4fa85e3cef3aec293
olcay <olcaytapan10@gmail.com>
2023-05-11 13:05:37 +0200

    coding-mail-service-59

--- Presentation/ETicaretAPI.API/Controllers/UsersController.cs
+++ Presentation/ETicaretAPI.API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
-﻿using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
+﻿using ETicaretAPI.Application.Abstraction.Services;
+using ETicaretAPI.Application.Features.Commands.AppUser.CreateUser;
 using ETicaretAPI.Application.Features.Commands.AppUser.FacebookLogin;
 using ETicaretAPI.Application.Features.Commands.AppUser.GoogleLogin;
 using ETicaretAPI.Application.Features.Commands.AppUser.LoginUser;
@@ -12,9 +13,12 @@ namespace ETicaretAPI.API.Controllers
     public class UsersController : ControllerBase
     {
         readonly IMediator _mediator;
-        public UsersController(IMediator mediator)
+        readonly IMailService _mailService;
+
+        public UsersController(IMediator mediator, IMailService mailService)
         {
             _mediator = mediator;
+            _mailService = mailService;
         }
 
         [HttpPost]
@@ -23,5 +27,13 @@ namespace ETicaretAPI.API.Controllers
             CreateUserCommandResponse response = await _mediator.Send(createUserCommandRequest);
             return Ok(response);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> ExampleMailTest()
+        {
+            await _mailService.SendMessageAsync("olcaytapan10@gmail.com", "olcaytapan10@gmail.com", "<strong>Bu bir örnek maildir.</strong>");
+            return Ok();
+        }
+
     }
 }
